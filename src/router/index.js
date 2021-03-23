115c559c3b0b6edaf4efb3091fe7f68343ca1d36
Mohamed Hesham <67207376+mohamedhesham221@users.noreply.github.com>
2021-03-23 02:17:13 +0200

    redirect file added

--- src/router/index.js
+++ src/router/index.js
@@ -6,64 +6,64 @@ import Products from "../views/Products.vue";
 import Blog from "../views/Blog.vue";
 import Contact from "../views/Contact.vue";
 import Gallery from "../views/Gallery.vue";
-import Employees from "../views/Employees.vue"
-import Movies from "../views/Movies.vue"
-import Profile from "../views/Profile.vue"
+import Employees from "../views/Employees.vue";
+import Movies from "../views/Movies.vue";
+import Profile from "../views/Profile.vue";
 // import { component } from "vue/types/umd";
 Vue.use(VueRouter);
 
 const routes = [
-    {
-        path: "/",
-        name: "Home",
-        component: Home
-    },
-    {
-        path: "/about",
-        name: "About",
-        component: About
-    },
-    {
-        path: "/products",
-        name: "Products",
-        component: Products
-    },
-    {
-        path:"/blog",
-        name: "Blog",
-        component: Blog
-    },
-    {
-        path: "/contact",
-        name: "Contact",
-        component: Contact    
-    },
-    {
-        path: "/gallery",
-        name: "Gallery",
-        component: Gallery
-    },
-    {
-        path: "/employees",
-        name: "Employees",
-        component: Employees
-    },
-    {
-        path: "/movies",
-        name: "Movies",
-        component: Movies
-    },
-    {
-        path: "/profile",
-        name: "Profile",
-        component: Profile
-    }
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About
+  },
+  {
+    path: "/products",
+    name: "Products",
+    component: Products
+  },
+  {
+    path: "/blog",
+    name: "Blog",
+    component: Blog
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    component: Contact
+  },
+  {
+    path: "/gallery",
+    name: "Gallery",
+    component: Gallery
+  },
+  {
+    path: "/employees",
+    name: "Employees",
+    component: Employees
+  },
+  {
+    path: "/movies",
+    name: "Movies",
+    component: Movies
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: Profile
+  }
 ];
 
 const router = new VueRouter({
-    mode: "history",
-    base: process.env.BASE_URL,
-    routes
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
 });
 
 export default router;
